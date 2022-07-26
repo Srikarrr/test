b138a3f376d55af45e9884905669ed689867fc14
koppalashanmukhasrikar <koppalashanmukhasrikar@gmail.com>
2022-07-26 10:53:04 +0530

    yes

---
+++ Kitten.java
@@ -0,0 +1,11 @@
+public class Kitten {
+    public void smuggle(){
+        System.out.println("x");
+    }
+    public void meow(){
+        System.out.println("y");
+    }
+    public void freakout(){
+        System.out.println("y");
+    }
+}
